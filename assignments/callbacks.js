8a475ccb26d2217e60ed5e9f0746762f41c4af5c
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-07-23 16:22:39 -0500

    completed MVPs for array

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -64,12 +64,13 @@ multiplyNums(3, 4, function(mult) {
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
   const count = items.length;
   for (var i = 0; i < count; i++) {
     if(list[i]===item){return cb(item)}
   }
   return false
-  // Pass true to the callback if it is, otherwise pass false.
+  
 };
 
 contains('yo-yo', items, function(stuff) {
